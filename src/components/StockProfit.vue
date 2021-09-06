a0e38b9f95abad3b07d3977cd7ba2160b73302bb
Matthew Webster <webstem1@airproducts.com>
2021-09-06 09:56:47 +0100

    new calculation and prettify

--- src/components/StockProfit.vue
+++ src/components/StockProfit.vue
@@ -3,12 +3,19 @@
     <h2>Stock Profit</h2>
     <ul>
       <li>
-        ( <input v-model="close" placeholder="close" size=4> / <input v-model="open" placeholder="open" size=4> ) x ( <input v-model="leverage" placeholder="leverage" size=4> x $<input v-model="investment" placeholder="investment" size=4> ) - ( <input v-model="leverage" placeholder="leverage" size=4> x $<input v-model="investment" placeholder="investment" size=4> )
-        <p>( {{close}} / {{open}} ) x ( {{leverage}} x ${{investment}} ) - ( {{leverage}} x ${{investment}} ) = ${{profit}}</p>
+        <p>( Close / Open ) x ( Leverage x Investment ) - ( Leverage x Investment ) = $</p>
+        <p>( <input v-model="sp_close" placeholder="close" size=4> / <input v-model="sp_open" placeholder="open" size=4> ) x ( <input v-model="sp_leverage" placeholder="leverage" size=4> x $<input v-model="sp_investment" placeholder="investment" size=4> ) - ( <input v-model="sp_leverage" placeholder="leverage" size=4> x $<input v-model="sp_investment" placeholder="investment" size=4> ) = ${{sp_profit}}</p>
+        <p>( {{sp_close}} / {{sp_open}} ) x ( {{sp_leverage}} x ${{sp_investment}} ) - ( {{sp_leverage}} x ${{sp_investment}} ) = ${{sp_profit}}</p>
       </li>
-      <br/>
+    </ul>
+  </div>
+  <div>
+    <h2>Percentage Increase</h2>
+    <ul>
       <li>
-        ( Close / Open ) x ( Leverage x Investment ) - ( Leverage x Investment )
+        <p>( ( Close - Open ) / Open ) * 100 = %</p>
+        <p>( ( <input v-model="pc_close" placeholder="close" size=4> - <input v-model="pc_open" placeholder="open" size=4> ) / <input v-model="pc_open" placeholder="pc_open" size=4> ) * 100 = {{pc_percentage}}%</p>
+        <p>( ( {{pc_close}} - {{pc_open}} ) / {{pc_open}} ) * 100 = {{pc_percentage}}%</p>
       </li>
     </ul>
   </div>
@@ -22,15 +29,20 @@ export default {
   },
   data () {
     return {
-      'close': 4486.98,
-      'open': 4377.46,
-      'leverage': 20,
-      'investment': 200
+      'sp_close': 4486.98,
+      'sp_open': 4377.46,
+      'sp_leverage': 20,
+      'sp_investment': 200,
+      'pc_open': 4377.46,
+      'pc_close': 4486.98
     }
   },
   computed: {
-    profit () {
-      return ((this.close / this.open) * (this.leverage * this.investment) - (this.leverage * this.investment)).toFixed(2)
+    sp_profit () {
+      return ((this.sp_close / this.sp_open) * (this.sp_leverage * this.sp_investment) - (this.sp_leverage * this.sp_investment)).toFixed(2)
+    },
+    pc_percentage () {
+      return (((this.pc_close-this.pc_open)/this.pc_open)*100).toFixed(2)
     }
   }
 }
